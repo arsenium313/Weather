771f30f4e75c0379c9b0281614dbe0f26a99a2dc
arsenium313 <arsenium313@gmail.com>
2023-07-05 16:01:57 +0300

    Delete storyboard

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -11,9 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
-
-
 }
 
